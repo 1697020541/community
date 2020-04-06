848ffe10290b2db6dae00e433cf983d04c8f1c8a
1697020541 <1697020541@qq.com>
2020-04-06 17:10:26 +0800

    add cookie

--- src/main/java/com/jinwan/community/controller/AuthorizeController.java
+++ src/main/java/com/jinwan/community/controller/AuthorizeController.java
@@ -10,7 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -23,14 +24,13 @@ public class AuthorizeController {
      @GetMapping("/callback")
     public  String  callback(@RequestParam(name= "code") String code,
                              @RequestParam(name="state") String state,
-                             HttpServletRequest  request){
+                             HttpServletResponse response){
          AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
          accessTokenDTO.setClient_id("Iv1.0ebcbb7268abcbd0");
          accessTokenDTO.setClient_secret("a7e65a3c28d295bef11e10554d002d9d09e14e97");
          accessTokenDTO.getRedirect_uri("http://localhost:8080/callback");
          accessTokenDTO.setState(state);
          accessTokenDTO.setCode(code);
-
          String accessToken = githubProvider.getAccessToken(accessTokenDTO);
          GithubUser githubUser = githubProvider.getUser(accessToken);
          if(githubUser !=null ){
@@ -40,13 +40,17 @@ public class AuthorizeController {
              user.setName(githubUser.getName());
              user.setGmtCreate(System.currentTimeMillis());
              user.setGmtModified(user.getGmtCreate());
-             user.setToken(UUID.randomUUID().toString());
-             System.out.println("nihao");
-             System.out.println(user);
+             String token = UUID.randomUUID().toString();
+             user.setToken(token);
              userMapper.insert(user);
-             request.getSession().setAttribute("user",user);
+             //登录成功，写cookie和session
+             Cookie cookie = new Cookie("token",token);
+             cookie.setMaxAge(60 * 60 * 24 * 30 * 6);
+             response.addCookie(cookie);
              return "redirect:/";
          }else {
+
+             //登录失败，重新登录
              return "redirect:/";
          }
 
